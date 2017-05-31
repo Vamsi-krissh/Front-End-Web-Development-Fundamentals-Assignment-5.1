6fa0270fbc9ea635c5a14e265cc8fb5c880146a0
Vamsi-krissh <vamsi1393@gmail.com>
2017-05-31 16:29:39 +0530

    Assignmnet 5.1

---
+++ app1.js
@@ -0,0 +1,10 @@
+
+function myfunction(){
+
+    var name = "vamsi";
+    var age=25;
+ var pob="hyderbad"
+ var dob ="13/07/1993"
+
+console.log("Name:"+ name+"\n"+"Age:"+age+"\n"+"Dob:"+dob+"\n"+"Place of birth:"+pob)
+};
